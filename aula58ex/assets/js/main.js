079822de4959124b38b2c4ae3d70ea9be52fa635
Gustavo Bento <133028031+GustavoBento@users.noreply.github.com>
2023-07-07 20:47:07 -0300

    aula 58

---
+++ aula58ex/assets/js/main.js
@@ -0,0 +1,7 @@
+const paragrafos = document.querySelector('.paragrafos')
+const ps = document.querySelectorAll('p')
+
+for (let p of ps) {
+    p.style.backgroundColor = 'rgb(17, 86, 102)'
+    p.style.color = 'white'
+}
